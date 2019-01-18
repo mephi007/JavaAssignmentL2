a152910fb5d7df9d3e35de82724be562f5f0f01e
Sumit Kumar Roy (Communications-Telecom Equipment) <SU20051907@wipro.com>
2019-01-18 12:21:10 +0530

    L2 TopicIV assignment1

---
+++ TopicIV/src/assignment1/ImplClassWithLambda.java
@@ -0,0 +1,29 @@
+package assignment1;
+
+public class ImplClassWithLambda {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String str = "How was your day";
+		char c = 'a';
+		
+		CharacterOccurence lambda =(String st, char l) -> {
+			int count=0;
+			for(int i=0 ; i<st.length() ; i++)
+			{												//Implementing Interface using lambda
+				if(l == str.charAt(i))
+				{
+					count++;
+				}
+			}
+			
+			return count;
+		};
+		
+		int result = lambda.findOccurence(str, c);
+		System.out.println(c+" occured "+result+" times.");
+
+	}
+
+}
